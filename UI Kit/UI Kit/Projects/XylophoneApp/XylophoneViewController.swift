b36f4aae0821adc61374de5e4d94a2e9ffe2283e
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-23 18:57:10 +0100

    Xylophone added

--- UI Kit/UI Kit/Projects/XylophoneApp/XylophoneViewController.swift
+++ UI Kit/UI Kit/Projects/XylophoneApp/XylophoneViewController.swift
@@ -22,6 +22,15 @@ class XylophoneViewController: UIViewController {
         print("Value: \(value)")
         
         playSound(soundName: value)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(soundName: String) {
