503917e540343895a0970d2f770f29d401b21eca
Nadir Shah <lonewolfnadhu@gmail.com>
2023-05-03 10:39:10 +0100

    Main screen ui updated

--- UI Kit/UI Kit/HomeViewController.swift
+++ UI Kit/UI Kit/HomeViewController.swift
@@ -14,6 +14,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        mainView.layer.cornerRadius = 30
+//        mainView.layer.cornerRadius = 30
     }
 }
